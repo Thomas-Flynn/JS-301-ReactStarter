484c422daa72e10e87ad889d1e4c026e6814e775
Thomas Flynn <thomasflynn@Thomass-MacBook-Pro.local>
2018-04-04 10:28:22 -0400

    commit 5-express deletes

--- 5-Express-Server/server/models/songlist.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-    let Songlist = sequelize.define('songlist', {
-        artist: DataTypes.STRING,
-        songTitle: DataTypes.STRING
-    })
-    return Songlist;
-};
